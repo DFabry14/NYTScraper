f46b7eadc3b5a62ec155ffe30e17b98a5f6d74ec
Dan Fabry <dfabry14@gmail.com>
2018-06-14 14:40:38 -0500

    initial site files

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+var db = require("./models");
+
+var PORT = 3000;
+
+var app = express();
+
+app.use(logger("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/nytscraper";
+mongoose.Promise = Promise;
+mongoose.connect(MONGODB_URI);
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
